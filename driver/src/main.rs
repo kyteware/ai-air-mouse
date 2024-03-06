a22d312546670e5c7284921bb945b5f1e026c4b6
Dworv YT <dwarvyt@gmail.com>
2024-03-05 20:54:50 -0800

    feat: output csv of outputs for reseaarch

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -1,24 +1,18 @@
 use std::{fs::File, io::{BufReader, Read}, process::exit, time::Instant};
 use byteorder::{LittleEndian, ByteOrder};
-
-#[derive(Debug)]
-struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32
-}
+use nalgebra::Vector3;
 
 #[derive(Debug)]
 struct Packet {
-    acc: Vec3,
-    gyr: Vec3
+    acc: Vector3<f32>,
+    gyr: Vector3<f32>
 }
 
 impl Packet {
     fn parse(raw: &[u8; 24]) -> Self {
         Self {
-            acc: Vec3 { x: LittleEndian::read_f32(&raw[0..4]), y: LittleEndian::read_f32(&raw[4..8]), z: LittleEndian::read_f32(&raw[8..12]) },
-            gyr: Vec3 { x: LittleEndian::read_f32(&raw[12..16]), y: LittleEndian::read_f32(&raw[16..20]), z: LittleEndian::read_f32(&raw[20..24]) }
+            acc: Vector3::new(LittleEndian::read_f32(&raw[0..4]), LittleEndian::read_f32(&raw[4..8]), LittleEndian::read_f32(&raw[8..12])),
+            gyr: Vector3::new(LittleEndian::read_f32(&raw[12..16]), LittleEndian::read_f32(&raw[16..20]), LittleEndian::read_f32(&raw[20..24]))
         }
     }
 }
@@ -31,11 +25,14 @@ fn main() {
         println!("this isn't a glove");
         exit(1);
     }
+
+    let mut orientation = Vector3::new(0.0, 0.0, 0.0);
     
     let mut err_buf = [0; 1];
     let mut packet_buf = [0; 24];
     let mut last_check: Option<Instant> = None;
-    loop {
+    println!("orientation x, orientation y, orientation z, accel x, accel y, accel z");
+    for _ in 0..10000 {
         dev.read_exact(&mut err_buf).unwrap();
         if err_buf[0] != 43 {
             println!("device error");
@@ -44,14 +41,14 @@ fn main() {
         dev.read_exact(&mut packet_buf).unwrap();
         let packet = Packet::parse(&packet_buf);
 
-        if let Some(last) = last_check {
-            let elapsed = last.elapsed();
-            if elapsed.as_millis() > 1000 {
-                println!("acc: {:?}, gyr: {:?}", packet.acc, packet.gyr);
-                last_check = Some(Instant::now());
-            }
-        } else {
-            last_check = Some(Instant::now());
+        if let Some(last_check) = last_check {
+            let elapsed = last_check.elapsed();
+
+            orientation += packet.gyr * elapsed.as_secs_f32();
         }
+
+        last_check = Some(Instant::now());
+
+        println!("{}, {}, {}, {}, {}, {}", orientation.x, orientation.y, orientation.z, packet.acc.x, packet.acc.y, packet.acc.z);
     }
 }
