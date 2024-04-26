121572b57ae8a0822d8c989c4f0f6ebfaea0c4b5
dworv <dwarvyt@gmail.com>
2024-04-26 08:03:53 -0700

    feat: attempt to smooth hand movements

--- src/main.py
+++ src/main.py
@@ -11,8 +11,10 @@ hands = mp_hands.Hands(max_num_hands=1)
 mp_draw = mp.solutions.drawing_utils
 
 pos = [300, 300]
-last_thumb_coords = [0, 0]
-delta_thumb_coords = [0, 0]
+est_thumb_pos = None
+est_index_pos = None
+est_thumb_log_pos = None
+est_index_log_pos = None
 
 while 1:
     ret, frame = cam.read()
@@ -20,20 +22,30 @@ while 1:
         x, y, _ = frame.shape
         frame = cv2.flip(frame, 1)
         result = hands.process(frame)
-        if result.multi_hand_landmarks:
-                thumb = result.multi_hand_landmarks[0].landmark[4]
-                delta_thumb_coords = (thumb.x - last_thumb_coords[0], thumb.y - last_thumb_coords[1])
-                last_thumb_coords = (thumb.x, thumb.y)
+        if result.multi_hand_landmarks: # this will skip if theres no hand
+            mp_draw.draw_landmarks(frame, result.multi_hand_landmarks[0], mp_hands.HAND_CONNECTIONS)
+            hand_log = result.multi_hand_landmarks[0].landmark
+            thumb_log = hand_log[4]
+            index_log = hand_log[8]
+            hand = result.multi_hand_world_landmarks[0].landmark
+            thumb = hand[4]
+            index = hand[8]
+            
+            if est_thumb_pos is None:
+                est_thumb_pos = [thumb.x, thumb.y, thumb.z]
+                est_index_pos = [index.x, index.y, thumb.z]
 
-                mp_draw.draw_landmarks(frame, result.multi_hand_landmarks[0], mp_hands.HAND_CONNECTIONS)
-        if result.multi_hand_world_landmarks:
-            thumb = result.multi_hand_world_landmarks[0].landmark[4]
-            index = result.multi_hand_world_landmarks[0].landmark[8]
-            dist = math.sqrt((abs(thumb.x - index.x)**2 + abs(thumb.y - index.y)**2 + abs(thumb.z - thumb.z)**2))
-            if dist < 0.03:
-                print(delta_thumb_coords)
-                pos[0] += delta_thumb_coords[0] * 1000
-                pos[1] += delta_thumb_coords[1] * 1000
+            est_thumb_pos[0] += (thumb.x - est_thumb_pos[0]) / 2
+            est_thumb_pos[1] += (thumb.y - est_thumb_pos[1]) / 2
+            est_thumb_pos[2] += (thumb.z - est_thumb_pos[2]) / 2
+            print(est_thumb_pos[2])
+
+            dist1 = math.sqrt((abs(est_index_pos[0] - est_thumb_pos[0])**2 + abs(est_index_pos[1] - est_thumb_pos[1])**2 + abs(est_index_pos[2] - est_thumb_pos[2])**2))
+            dist = math.sqrt((abs(thumb.x - index.x)**2 + abs(thumb.y - index.y)**2 + abs(thumb.z - index.z)**2))
+
+            # print(dist1 - dist)
+            # if dist < 0.08:
+            # print("touching", dist)
         
         frame = cv2.circle(frame, (int(pos[0]), int(pos[1])), 25, (255,255,0), 3)
         cv2.imshow("capstone", frame)
