30e3d579d9ae96da5d1fc0e654e3e105613dd93f
dworv <dwarvyt@gmail.com>
2024-05-09 16:50:12 -0700

    feat: begin gesture recognition

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import mediapipe as mp
+from mediapipe.tasks.python import vision, BaseOptions
 import numpy as np
 import cv2
 import math
+from time import time
 
 cam = cv2.VideoCapture(0)
 win = cv2.namedWindow("capstone")
@@ -10,18 +12,27 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1)
 mp_draw = mp.solutions.drawing_utils
 
+def callback(x, y, z):
+    print(type(x), type(y), type(z))
+
+base_options = BaseOptions("ai-model/export/gesture_recognizer.task")
+options = vision.GestureRecognizerOptions(base_options, running_mode=vision.RunningMode.LIVE_STREAM, result_callback=lambda x, y, z: print("got a callback", x, y, z))
+recognizer = vision.GestureRecognizer.create_from_options(options)
+
 pos = [300, 300]
 est_thumb_pos = None
 est_index_pos = None
 est_thumb_log_pos = None
 est_index_log_pos = None
 
+start = math.floor(time() * 1000)
+
 while 1:
     ret, frame = cam.read()
     if ret:
-        x, y, _ = frame.shape
         frame = cv2.flip(frame, 1)
-        result = hands.process(frame)
+        img = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        res = recognizer.recognize_async(img, math.floor(time() * 1000) - start)
         if result.multi_hand_landmarks: # this will skip if theres no hand
             mp_draw.draw_landmarks(frame, result.multi_hand_landmarks[0], mp_hands.HAND_CONNECTIONS)
             hand_log = result.multi_hand_landmarks[0].landmark
