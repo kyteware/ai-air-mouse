a48bdad9958fc9ba6b2dfaaef826539429e3bad7
dworv <dwarvyt@gmail.com>
2024-05-13 18:33:57 -0700

    feat: gesture recognition full support (no more window though)

--- src/main.py
+++ src/main.py
@@ -6,24 +6,32 @@ import math
 from time import time
 
 cam = cv2.VideoCapture(0)
-win = cv2.namedWindow("capstone")
 
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(max_num_hands=1)
-mp_draw = mp.solutions.drawing_utils
+pos = [500., 500.]
+old_thumb_loc = None
+streak = 0
 
-def callback(x, y, z):
-    print(type(x), type(y), type(z))
+def callback(res, img, time):
+    global old_thumb_loc, streak
+    if res.gestures:
+        gesture = res.gestures[0][0]
+        thumb_loc = [res.hand_landmarks[0][5].x, res.hand_landmarks[0][5].y]
+        if old_thumb_loc and gesture.category_name == "touching":
+            streak += 1
+            delta_thumb = [thumb_loc[0] - old_thumb_loc[0], thumb_loc[1] - old_thumb_loc[1]]
+            pos[0] += delta_thumb[0]
+            pos[1] += delta_thumb[1]
+        else:
+            print(streak)
+            if streak < 100 and streak > 20:
+                print("left click")
+            streak = 0
+        old_thumb_loc = thumb_loc
+    # print(pos)
 
 base_options = BaseOptions("ai-model/export/gesture_recognizer.task")
-options = vision.GestureRecognizerOptions(base_options, running_mode=vision.RunningMode.LIVE_STREAM, result_callback=lambda x, y, z: print("got a callback", x, y, z))
+options = vision.GestureRecognizerOptions(base_options, running_mode=vision.RunningMode.LIVE_STREAM, result_callback=callback)
 recognizer = vision.GestureRecognizer.create_from_options(options)
-
-pos = [300, 300]
-est_thumb_pos = None
-est_index_pos = None
-est_thumb_log_pos = None
-est_index_log_pos = None
 
 start = math.floor(time() * 1000)
 
@@ -32,35 +40,7 @@ while 1:
     if ret:
         frame = cv2.flip(frame, 1)
         img = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-        res = recognizer.recognize_async(img, math.floor(time() * 1000) - start)
-        if result.multi_hand_landmarks: # this will skip if theres no hand
-            mp_draw.draw_landmarks(frame, result.multi_hand_landmarks[0], mp_hands.HAND_CONNECTIONS)
-            hand_log = result.multi_hand_landmarks[0].landmark
-            thumb_log = hand_log[4]
-            index_log = hand_log[8]
-            hand = result.multi_hand_world_landmarks[0].landmark
-            thumb = hand[4]
-            index = hand[8]
-            
-            if est_thumb_pos is None:
-                est_thumb_pos = [thumb.x, thumb.y, thumb.z]
-                est_index_pos = [index.x, index.y, thumb.z]
-
-            est_thumb_pos[0] += (thumb.x - est_thumb_pos[0]) / 2
-            est_thumb_pos[1] += (thumb.y - est_thumb_pos[1]) / 2
-            est_thumb_pos[2] += (thumb.z - est_thumb_pos[2]) / 2
-            print(est_thumb_pos[2])
-
-            dist1 = math.sqrt((abs(est_index_pos[0] - est_thumb_pos[0])**2 + abs(est_index_pos[1] - est_thumb_pos[1])**2 + abs(est_index_pos[2] - est_thumb_pos[2])**2))
-            dist = math.sqrt((abs(thumb.x - index.x)**2 + abs(thumb.y - index.y)**2 + abs(thumb.z - index.z)**2))
-
-            # print(dist1 - dist)
-            # if dist < 0.08:
-            # print("touching", dist)
-        
-        frame = cv2.circle(frame, (int(pos[0]), int(pos[1])), 25, (255,255,0), 3)
-        cv2.imshow("capstone", frame)
-        cv2.waitKey(1)
+        recognizer.recognize_async(img, math.floor(time() * 1000) - start)
     else:
         print("broke")
         break
